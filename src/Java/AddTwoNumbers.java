37ea8ba788540eba90d67acf954a6cdafa14bdd8
shivamp1998 <shivampandey98@gmail.com>
2022-10-26 12:55:23 +0530

    [Salary]

--- src/Java/AddTwoNumbers.java
+++ src/Java/AddTwoNumbers.java
@@ -1,12 +1,30 @@
 package Java;
-
+import java.util.Scanner;
+import java.lang.Math;
 public class AddTwoNumbers {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		int a = 6,b=7;
-		System.out.println(a+b);
-
+	
+	public static int totalSalary(int basic, char grade) {
+        double hra = basic * 20.0 / 100;
+        double da = basic * 50.0 / 100;
+        double pf = basic * 11.0 / 100;
+        double salary = hra + da - pf + basic;
+        if(grade == 'A') {
+            salary += 1700;
+		}else if(grade == 'B') {
+            salary += 1500;
+        }else{
+            salary += 1300;
+        }
+        return (int)Math.round(salary);
+    }
+	
+    public static void main(String[] args) {
+		// Write your code here
+        
+        Scanner sc = new Scanner(System.in);
+        int basic = sc.nextInt();
+        char grade = sc.next().charAt(0);
+		int total = totalSalary(basic,grade);
+        System.out.println(total);
 	}
-
 }
