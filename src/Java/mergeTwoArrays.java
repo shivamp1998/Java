7facc3bfa34d174ae72d30ba90375a653af7898d
shivamp1998 <shivampandey98@gmail.com>
2022-10-30 02:00:14 +0530

    changes

--- src/Java/mergeTwoArrays.java
+++ src/Java/mergeTwoArrays.java
@@ -1,31 +1,28 @@
 package Java;
 public class mergeTwoArrays {
-    public static int[] mergeTwoArrays(int[] arr1, int[] arr2) {
-        int i = 0 , j = 0;
+    public static void main(String ...args) {
+        int arr1[] = new int []{1,2,3,5};
+        int arr2[] = new int []{4,6};
+        int i1 = 0 , j = 0;
         int n = arr1.length;
         int m = arr2.length;
         int k = 0;
         int newArray[] = new int [m+n];
-        while(i<n && j<m) {
-            if(arr1[i] < arr2[j]) {
-                newArray[k++] = arr1[i++];
+        while(i1<n && j<m) {
+            if(arr1[i1] < arr2[j]) {
+                newArray[k++] = arr1[i1++];
             }else{
                 newArray[k++] = arr2[j++];
             }
         }
-
-        while(i < n) {
-            newArray[k++] = arr1[i++];
+        
+        while(i1 < n) {
+            newArray[k++] = arr1[i1++];
         }
         while(j < m) {
             newArray[k++] = arr2[j++];
         }
-        return newArray;
-    }
-    public static void main(String ...args) {
-        int arr1[] = new int []{1,2,3,5};
-        int arr2[] = new int []{4,6};
-        int[] finalArray = mergeTwoArrays(arr1, arr2); 
+        int[] finalArray = newArray; 
         
         for(int i=0; i<finalArray.length; i++) {
             System.out.print(finalArray[i]);
