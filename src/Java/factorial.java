014057b7211200cf37aad476a9b4e17e47f58a8a
shivamp1998 <shivampandey98@gmail.com>
2022-11-09 04:18:53 +0530

    [fix]

---
+++ src/Java/factorial.java
@@ -0,0 +1,10 @@
+package Java;
+public class factorial {
+    public static int fact(int n) {
+        if(n == 1 || n == 0 ) return 1;
+        return n*fact(n-1);
+    }
+    public static void main(String ...args) {
+        System.out.print(fact(5));
+    }
+}
