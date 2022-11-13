e19437b3a60566f9cba718fffb2e4d5eb4131b45
shivamp1998 <shivampandey98@gmail.com>
2022-11-13 21:22:18 +0530

    [StringRecursion]

--- src/Java/stringRecursion.java
+++ src/Java/stringRecursion.java
@@ -7,6 +7,13 @@ public class stringRecursion {
         if(input.charAt(0) == 'x') return s;
         else return s + input.charAt(0);
     }
+
+    public static String removeDuplicates(String s) {
+        if(s.length() <= 1) return s;
+        String ob = removeDuplicates(s.substring(1));
+        if(s.charAt(0) == s.charAt(1)) return ob;
+        else return s.charAt(0) + ob;
+    }
     public static void main(String...args) {
         System.out.print(recur("pxxp"));
     }   
