ef0927fffc08eff0c3c19c27944ffa794440b64f
shivamp1998 <shivampandey98@gmail.com>
2022-11-29 21:05:56 +0530

    [Fix]

--- src/Java/test.java
+++ src/Java/test.java
@@ -1,21 +1,12 @@
 package Java;
-class Vehicle{
-    void print(){
-    System.out.print("Vehicle class");
-    }
-}
-class Car extends Vehicle{
-    void print(){
-    System.out.print("Car class ");
-    }
-}
+class test{
+    public static void main(String[] args){
 
-public class test {
-    public static void main(String[] args){   
-    Vehicle obj1 = new Car();
-    obj1.print();
-
-    Vehicle obj2 = new Vehicle();
-    obj2.print();  
+    try{
+       int a = 5/0;
     }
+    finally{
+        
+    }
+ }
 }
