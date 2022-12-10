b0762244ce64c263f9cec469ab4ed70b3c4b0ab5
shivamp1998 <shivampandey98@gmail.com>
2022-12-10 20:26:46 +0530

    [eventodd]

---
+++ src/Java/evenOddll.java
@@ -0,0 +1,34 @@
+package Java;
+
+public class evenOddll {
+    public static LinkedListNode<Integer> evenAfterOdd(LinkedListNode<Integer> head) {
+		LinkedListNode<Integer> evenStart = null , evenEnd = null;
+        LinkedListNode<Integer> oddStart = null, oddEnd = null;
+        while(head!=null) {
+            if(head.data % 2 == 0 ) {
+                if(evenEnd == null) {
+                    evenStart = head;
+                    evenEnd = head;
+                }else{
+                    LinkedListNode<Integer> temp = new LinkedListNode<>(head.data);
+                    evenEnd.next = temp;
+                    evenEnd = temp;
+                }
+            }else{
+                if(oddEnd == null) {
+                    oddStart = head;
+                    oddEnd = head;
+                }else{
+                    LinkedListNode<Integer> temp = new LinkedListNode<Integer>(head.data);
+                    oddEnd.next = temp;
+                    oddEnd = temp;
+                }
+            }
+            head = head.next;
+            
+        }
+        oddEnd.next = evenEnd;
+        return oddStart;
+
+	}
+}
