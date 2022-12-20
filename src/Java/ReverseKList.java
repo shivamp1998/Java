5f161d397007a540362c8e7880dfb50bb4cfc4c9
shivamp1998 <shivampandey98@gmail.com>
2022-12-20 11:26:29 +0530

    [reverseKNodes]

--- src/Java/ReverseKList.java
+++ src/Java/ReverseKList.java
@@ -1,20 +1,24 @@
 package Java;
 
 public class ReverseKList {
+
+    
     public static Node kReverse(Node head, int k) {
-        if(k == 0 || k == 1 ) return head;
-        if(head == null || head.next == null) return head;
-        Node prev = null, curr = head, next = head.next;
-        while(--k > 0 && next != null) {
+        if(k == 0 || k == 1) return head;
+        Node curr = head;
+        Node next = null;
+        Node prev = null;
+        int count = k;
+        while(curr != null && count-- > 0) {
+            next = curr.next;
             curr.next = prev;
             prev = curr;
             curr = next;
-            next = next.next;
         }
-        curr.next = prev;
-        Node temp  = next;
-        kReverse(next, k);
-        temp.next = curr;
-        return curr;
+        if(next != null) {
+            head.next = kReverse(next, k);
+        }
+        return prev;
+        
     }  
 }
