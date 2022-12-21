66659c8f1ef6abb6d487c4d36b47232f9e862fc2
shivamp1998 <shivampandey98@gmail.com>
2022-12-21 20:58:48 +0530

    [BubbleSort]

---
+++ src/Java/bubbleSortLL.java
@@ -0,0 +1,32 @@
+package Java;
+
+public class bubbleSortLL {
+    public static void swap(Node head, Node t1, Node t2) {
+        Node prev1 = null, curr1 = head;
+        
+    }
+    public static Node sort(Node head) {
+        int len = length(head);
+        if(head == null || head.next == null) return head;
+        Node curr = head, next = null;
+        for(int i=0; i<len; i++) {
+            while(curr!=null) {
+                next = curr.next;
+                if(curr.data > next.data) {
+                    swap(curr,next);
+                }
+            }
+        }
+        return head;
+    }
+    public static int length(Node head) {
+        int count = 0;
+        Node temp = head;
+        while(temp != null) {
+            temp = temp.next;
+            count++;
+        }
+        return count;
+    }
+
+}
