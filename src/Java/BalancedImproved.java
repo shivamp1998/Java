8e08051cb46d8500e5d7bab3814d76f39591865c
shivamp1998 <shivampandey98@gmail.com>
2022-12-25 12:55:30 +0530

    [BalancedTree]

---
+++ src/Java/BalancedImproved.java
@@ -0,0 +1,10 @@
+package Java;
+
+public class BalancedImproved {
+    int height;
+    boolean isbalanced;
+    BalancedImproved(int height, boolean balanced) {
+        this.height = height;
+        isbalanced = balanced;
+    }
+}
--- src/Java/BinaryTreeUse.java
+++ src/Java/BinaryTreeUse.java
@@ -16,6 +16,44 @@ public class BinaryTreeUse {
         return NumberOfLeafs(root.left) + NumberOfLeafs(root.right);
     }
 
+    public static int height(BinaryTreeNode<Integer> root) {
+        if(root == null) return 0;
+        return 1 + Math.max(height(root.left), height(root.right));
+    }
+
+    public static boolean balancedTree(BinaryTreeNode<Integer> root) {
+        if(root == null) return true;
+        int left = height(root.left);
+        int right = height(root.right);
+        if(Math.abs(left- right) > 1) return false;
+        boolean isLeftBalanced = balancedTree(root.left);
+        boolean isRightBalanced = balancedTree(root.right);
+        return isLeftBalanced && isRightBalanced;
+    }
+
+    public static BalancedImproved imporvedBalanced(BinaryTreeNode<Integer> root) {
+        if(root == null) {
+            BalancedImproved ans = new BalancedImproved(0, true);
+            return ans;
+        }
+        BalancedImproved left = imporvedBalanced(root.left);
+        BalancedImproved right = imporvedBalanced(root.right);
+        int height = 1 + Math.max(left.height, right.height);
+        boolean isBal = true;
+        if(Math.abs(left.height - right.height) > 1) {
+            isBal = false;
+        }
+        if(!left.isbalanced || !right.isbalanced) {
+            isBal = false;
+        }
+        BalancedImproved ans = new BalancedImproved(height, isBal);
+        return ans;
+
+
+    }
+
+    
+
     public static BinaryTreeNode<Integer>  takeInput() {
         Scanner sc = new Scanner(System.in);
         int data = sc.nextInt();
