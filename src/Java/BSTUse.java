420c32655af32eed6d5eb169161d917dc5d7fef5
shivamp1998 <shivampandey98@gmail.com>
2022-12-31 19:32:49 +0530

    [fix]

--- src/Java/BSTUse.java
+++ src/Java/BSTUse.java
@@ -31,6 +31,55 @@ public class BSTUse {
             print(root.right);
         }
     }
+    public static BinaryTreeNode<Integer> sortedArrayHelper(int[] arr, int n, int i, int j) {
+        if(i > j) return null;
+        int mid = (i + j)/2;
+        BinaryTreeNode<Integer> root = new BinaryTreeNode<Integer>(arr[mid]);
+        root.left = sortedArrayHelper(arr, n, i, mid-1);
+        root.right = sortedArrayHelper(arr, n, mid+1, j);
+        return root;
+    }
+    public static BinaryTreeNode<Integer> SortedArrayToBST(int[] arr, int n){
+        BinaryTreeNode<Integer> root = sortedArrayHelper(arr, n, 0, arr.length-1);
+        return root;
+    }
+
+    public static isBstReturn isBstImprovved(BinaryTreeNode<Integer> root) {
+        if(root == null) {
+            return new isBstReturn(Integer.MAX_VALUE, Integer.MIN_VALUE, true);
+        }
+        isBstReturn left = isBstImprovved(root.left);
+        isBstReturn right = isBstImprovved(root.right);
+
+        int min = Math.min(root.data, Math.min(left.min, right.min));
+        int max = Math.max(root.data, Math.max(left.max, right.max));
+        
+        boolean isBst = true;
+        if(left.max >= root.data) {
+            isBst = false;
+        }
+        if(right.min < root.data) {
+            isBst = false;
+        }
+        if(!right.isBst) {
+            isBst = false;
+        }
+
+        if(!left.isBst) {
+            isBst = false;
+        }
+        return new isBstReturn(max, min, isBst);
+        
+    }
+
+    public static boolean isBstMaxImproved(BinaryTreeNode<Integer> root, int minRange, int maxRange) {
+        if(root == null) return true;
+        
+        if(root.data > minRange && root.data < maxRange) {
+            return isBstMaxImproved(root.left, minRange, root.data-1) || isBstMaxImproved(root, root.data, maxRange);
+        }else return false;
+
+    }
 
     public static void elementsInRangeK1K2(BinaryTreeNode<Integer> root,int k1,int k2){
 
---
+++ src/Java/isBstReturn.java
@@ -0,0 +1,12 @@
+package Java;
+
+public class isBstReturn {
+    int max;
+    int min;
+    boolean isBst;
+    isBstReturn(int max, int min, boolean isBst) {
+        this.max = max;
+        this.min = min;
+        this.isBst = isBst;
+    }
+}
