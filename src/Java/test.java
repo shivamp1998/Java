b125cbd9c6079225d22932c8bc02712c3cb0dd68
shivamp1998 <shivampandey98@gmail.com>
2023-01-08 19:34:23 +0530

    [Java]

--- src/Java/test.java
+++ src/Java/test.java
@@ -3,13 +3,32 @@ import java.util.LinkedList;
 import java.util.Queue;
 import java.util.Stack;
 class test {
-    public static void main (String[] args) {
-        Queue<Integer> q=new LinkedList<Integer>();
-        Stack<Integer> s=new Stack<Integer>();
-        q.add(5);
-        q.add(10);
-        s.push(10);
-        s.push(20);
-        System.out.print(q.peek()+s.pop());
-    }
+    public static String[] subsequences(String s) {
+		if(s.length() == 0 ) {
+			String[] str = {""};
+			return str;
+		}
+		String[] smOutput = subsequences(s.substring(1));
+		String output[] = new String [smOutput.length * 2];
+		for(int i=0; i<smOutput.length; i++) {
+			output[i] = smOutput[i];
+		}
+		for(int i=0; i<smOutput.length; i++) {
+			output[i + smOutput.length] = s.charAt(0) + smOutput[i];
+		}
+
+		return output;
+
+	}
+	public static String[] keypad(int n){
+		// Write your code here
+		String ans[] = subsequences("abc");
+		return ans;
+	}
+    public static void main(String ...args) {
+        String[] ans = keypad(10);
+        for(int i=0; i<ans.length; i++) {
+            System.out.print(ans[i] + " ");
+        }
+    } 
 }
