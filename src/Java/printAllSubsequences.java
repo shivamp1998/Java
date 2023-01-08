3526fd6594ca80fe447a07eec63273e766ad28b1
shivamp1998 <shivampandey98@gmail.com>
2023-01-08 17:45:40 +0530

    fix

---
+++ src/Java/printAllSubsequences.java
@@ -0,0 +1,12 @@
+package Java;
+
+public class printAllSubsequences {
+    public static String print(String s, String ans) {
+        if(s.length() == 0) return " ";
+        return "";
+        // print(s.substring(1, 0))
+    }   
+    public static void main(String...args) {
+
+    }   
+}
