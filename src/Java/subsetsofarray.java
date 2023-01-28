cb1f8ca8ad847f5727c17cf341ad75c7716099a5
shivamp1998 <shivampandey98@gmail.com>
2023-01-28 11:40:19 +0530

    [Subsets of array]

---
+++ src/Java/subsetsofarray.java
@@ -0,0 +1,20 @@
+package Java;
+import java.util.*;
+public abstract class subsetsofarray {
+    public static void subsets(int arr[], int startIndex, String ans) {
+        if(startIndex == arr.length) {
+            System.out.println(ans);
+            return;
+        }
+        subsets(arr, startIndex+1, ans);
+        subsets(arr, startIndex+1, ans + arr[startIndex]);
+    }   
+    public static void main(String...args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int arr[] = new int [n];
+        for(int i=0; i<n; i++) arr[i] = sc.nextInt();
+
+        subsets(arr, 0, "");
+    }
+}
