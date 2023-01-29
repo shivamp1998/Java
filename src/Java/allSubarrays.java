c4cca2aac820f637a06bab184e2bca8da46c7da4
shivamp1998 <shivampandey98@gmail.com>
2023-01-29 22:36:09 +0530

    [all subsets]

---
+++ src/Java/allSubarrays.java
@@ -0,0 +1,20 @@
+package Java;
+
+public class allSubarrays {
+    public static int[][] helper(int input[], int k, int index) {
+		if(index == input.length -1) {
+            int arr[][] = {{}};
+			return arr;
+		}
+        int num = input[index];
+        int ans[][] = {{}};
+        return ans;
+    }
+	public static int[][] subsetsSumK(int input[], int k) {
+		// Write your code here
+        return helper(input,k,0);   
+	}
+    public static void main(String...args) {
+
+    }    
+}
