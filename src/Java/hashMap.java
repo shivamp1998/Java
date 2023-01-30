9cfead9ff873a1cdf9f5ed4873a58bd1569c0238
shivamp1998 <shivampandey98@gmail.com>
2023-01-30 22:03:01 +0530

    [Hashmap]

---
+++ src/Java/hashMap.java
@@ -0,0 +1,14 @@
+package Java;
+import java.util.HashMap;
+public class hashMap {
+    public static void main(String...args) {
+        HashMap<String, Integer> m = new HashMap<>();
+        m.put("abc",1);
+        if(m.containsKey("abc")) {
+            System.out.println("abc present")
+        }
+        //null pointer exception
+        int v1 = m.get("abc3");
+        System.out.println(m.size());
+    }
+}
