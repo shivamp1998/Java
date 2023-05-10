dfa71b6aa049adf4c5498994880344f5af8e8a68
shivamp1998 <shivampandey98@gmail.com>
2023-05-10 21:42:10 +0530

    duplicatell

--- src/Java/duplicatesLL.java
+++ src/Java/duplicatesLL.java
@@ -1,6 +1,12 @@
 package Java;
 
 public class duplicatesLL {
+    public static LinkedListNode<Integer> alterNateApproach(LinkedListNode<Integer> head) {
+        if(head == null) return null;
+        head.next = alterNateApproach(head.next);
+        if(head.next != null && head.data.equals(head.next.data)) return head.next;
+        return head;
+    }
     public static LinkedListNode<Integer> removeDuplicates(LinkedListNode<Integer> head) {
         LinkedListNode<Integer> curr = head;
         while(curr != null && curr.next != null) {
