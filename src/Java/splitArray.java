14386f90baac45de8beb4e7092e53f6110cf9afe
shivamp1998 <shivampandey98@gmail.com>
2023-05-20 21:03:17 +0530

    splitArray

---
+++ src/Java/splitArray.java
@@ -0,0 +1,21 @@
+package Java;
+
+public class splitArray {
+    public static boolean splitArray(int[] input, int group1, int group2, int index) {
+        if(input.length == index) {
+            if(group1 == group2) return true;
+            else return false;
+        }
+        if(input[index] % 3 == 0) {
+            return splitArray(input, group1 + input[index], group2, index)
+        }else if(input[index] % 5 == 0) {
+            return splitArray(input, group1, group2 + input[index], index);
+        }else {
+            return splitArray(input, group1 + input[index] , group2, index) || splitArray(input, group1, group2 + input[index], index);
+        }
+    }
+    public static void main(String...args) {
+        int [] input = new int [10];
+        return splitArray(input, 0, 0, 0)
+    }       
+}
