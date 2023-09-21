b90bcd5acad1f272c21d3634e031c2092a05923c
shivamp1998 <shivampandey98@gmail.com>
2023-09-21 22:52:48 +0530

    fixes

--- src/Java/isBst.java
+++ src/Java/isBst.java
@@ -22,6 +22,30 @@ public class isBst {
         return isTreeBst(root.left) && isTreeBst(root.right);
         
     }
+
+    public static isBstReturn improvedBsthelper(BinaryTreeNode<Integer> root) {
+        if(root == null) {
+            return new isBstReturn(Integer.MIN_VALUE,Integer.MAX_VALUE, true);
+        }
+        isBstReturn bstLeft = improvedBsthelper(root.left);
+        isBstReturn bstRight = improvedBsthelper(root.right);
+        int max = Math.max(root.data, Math.max(bstLeft.max, bstRight.max));
+        int min = Math.min(root.data, Math.min(bstLeft.min, bstRight.min));
+        boolean isBst = true;
+        if(max > root.data || min < root.data) {
+            isBst = false;
+        } 
+        isBst = isBst && bstLeft.isBst && bstRight.isBst;
+        return new isBstReturn(max, min, isBst);
+    }
+
+    public static boolean isBstImproved(BinaryTreeNode<Integer> root) {
+        isBstReturn a = new isBstReturn(Integer.MIN_VALUE, Integer.MAX_VALUE, true);
+        isBstReturn b = improvedBsthelper(root, a);
+        return b.isBst;
+    }   
+
+
     public static void main(String...args) {
 
     }    
