a1b58501c85b5af908f99e07fedf595ae92f532e
shivamp1998 <shivampandey98@gmail.com>
2023-09-27 21:34:43 +0530

    bst class

---
+++ src/Java/BST.java
@@ -0,0 +1,23 @@
+package Java;
+
+public class BST {
+    private BinaryTreeNode<Integer> root;
+    private int size;
+    public boolean isPresent( int x) {
+        return false;
+    }
+
+    public void insert( int x) {
+        return;
+    }
+
+    public boolean deleted( int x ) {
+        return false;
+    }
+
+    public int size() {
+        return 0;
+    }
+    
+
+}
