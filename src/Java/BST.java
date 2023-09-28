060bcb11e43db467fc2951a45d7ae7d016f5f06f
shivamp1998 <shivampandey98@gmail.com>
2023-09-28 10:01:30 +0530

    bst ispresent and printtree

--- src/Java/BST.java
+++ src/Java/BST.java
@@ -1,9 +1,15 @@
 package Java;
-
+import java.util.*;
 public class BST {
     private BinaryTreeNode<Integer> root;
     private int size;
-    public boolean isPresent( int x) {
+
+    public boolean isPresentHelper(BinaryTreeNode<Integer> root, int x) {
+        if(root == null) return false;
+        if(root.data == x) return true;
+        return isPresentHelper(root.left, x) || isPresentHelper(root, x);
+    }
+    public boolean isPresent(int x) {
         return false;
     }
 
@@ -17,6 +23,17 @@ public class BST {
 
     public int size() {
         return 0;
+    }
+
+    public void printTreeHelper(BinaryTreeNode<Integer> root) {
+        if(root == null) return;
+        printTreeHelper(root.left);
+        System.out.println(root.data);
+        printTreeHelper(root.right);
+    } 
+
+    public void printTree() {
+        printTreeHelper(root);
     }
     
 
