6bcca94e5b11099469823b05adc564f4c6366452
shivamp1998 <shivampandey98@gmail.com>
2023-10-04 21:59:22 +0530

    treenode

---
+++ src/Java/TreeNode.java
@@ -0,0 +1,13 @@
+package Java;
+import java.util.*;
+
+class TreeNode<T> {
+		T data;
+		ArrayList<TreeNode<T>> children;
+
+		TreeNode(T data){
+			this.data = data;
+			children = new ArrayList<TreeNode<T>>();
+		}
+}
+
