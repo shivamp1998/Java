bff39601d725ae3dadc2cfa1395a6a7deea437a8
shivamp1998 <shivampandey98@gmail.com>
2023-10-07 21:07:49 +0530

    fix

--- src/Java/minCount.java
+++ src/Java/minCount.java
@@ -1,12 +1,11 @@
 package Java;
-import java.util.*;
 public class minCount {
-    public static int minCount(int n) {
+    public static int minimumCount(int n) {
         if(n <= 3) return n;
         int res = n;
         for(int i=1; i<=n; i++) {
             if(i*i > n) break;
-            res = Math.min(res, 1 + minCount(n-i*i));
+            res = Math.min(res, 1 + minimumCount(n-i*i));
         }
         return res;
     }
