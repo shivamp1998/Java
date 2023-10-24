8eda1b7eb753a569a360e6372221ec09fe60275b
shivamp1998 <shivampandey98@gmail.com>
2023-10-24 20:26:46 +0530

    fix

--- src/Oops/staticVariable.java
+++ src/Oops/staticVariable.java
@@ -1,8 +1,6 @@
 package Oops;
 public class staticVariable {
     public static void main(String...args) {
-        Student s1 = new Student("shivam",1);
-        Student s2 = new Student("priyam",2);
         System.out.println(Student.StudentNums); //static variable accessing
         
     }
