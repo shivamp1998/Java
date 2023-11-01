21b2c6defc820860cc125f861329b4a4472623c2
shivamp1998 <shivampandey98@gmail.com>
2023-11-01 10:22:21 +0530

    max loot

--- src/Java/maxLootHouses.java
+++ src/Java/maxLootHouses.java
@@ -1,6 +1,16 @@
 package Java;
 
 public class maxLootHouses {
+    public static int iterativeDP(int houses[]) {
+        int n = houses.length;
+        int maxLoot[] = new int [n+1];
+        maxLoot[0] = houses[0];
+        maxLoot[1] = houses[1];
+        for(int i=2; i<n; i++) {
+            maxLoot[i] = Math.max(maxLoot[i-2] + houses[i], houses[i-1]);
+        }
+        return maxLoot[n+1];
+    }
     public static int maxHouses(int houses[], int si, int ans) {
         if(houses.length == si) {
             return ans;
