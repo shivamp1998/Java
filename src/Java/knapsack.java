3b970f48e0186bd5a251abe513fa8e3aadd163fd
shivamp1998 <shivampandey98@gmail.com>
2023-11-28 12:55:30 +0530

    knapsack iterative

--- src/Java/knapsack.java
+++ src/Java/knapsack.java
@@ -12,6 +12,26 @@ public class knapsack {
 			return Math.max(a1, a2);
 		}
 	}
+
+    public static int knapsackIterative(int wt[], int v[], int n, int w, int i) {
+        int dp[][] = new int [n+1][w+1];
+        
+        for(int j=n-1; j>=0; j--) {
+            int ans;
+            for(int k=0; k<=w;k++) {
+                if(wt[k] <= w) {
+                    int a1 = v[k] + dp[j+1][k - wt[j]];
+                    int a2 = dp[j+1][k];
+                    ans = Math.max(a1, a2);
+                }else{
+                    ans = dp[j+1][k];
+                }
+                dp[j][k] = ans;
+            }
+        }
+        return dp[0][w];
+    }
+
 	public static int knapsack(int[] weights, int[] values, int n, int maxWeight) {
 		return helper(weights, values, n, maxWeight, 0);
 	}
