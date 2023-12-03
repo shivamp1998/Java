08b3fc293351b538c678d3beae383adc9453fa27
shivamp1998 <shivampandey98@gmail.com>
2023-12-03 13:11:04 +0530

    pair sum changes

--- src/Java/pairSum.java
+++ src/Java/pairSum.java
@@ -1,28 +1,30 @@
 package Java;
+
 import java.util.*;
+
 public class pairSum {
-    public static int PairSum(int[] input, int size) {
+	public static int PairSum(int[] input, int size) {
 		int count = 0;
-		HashMap<Integer,Integer> m = new HashMap();
-		for(int i=0; i<input.length; i++) {
-			if(m.containsKey(input[i])) {
+		HashMap<Integer, Integer> m = new HashMap();
+		for (int i = 0; i < input.length; i++) {
+			if (m.containsKey(input[i])) {
 				m.put(input[i], m.get(input[i]) + 1);
-			}else{
+			} else {
 				m.put(input[i], 1);
 			}
 		}
-		for(int i=0; i<input.length; i++) {
-			if(m.containsKey(-input[i])) {
-				count += m.get(input[i]) * m.get(-input[i]);
+
+		for (int x : m.keySet()) {
+			if (x != 0 && m.containsKey(-x)) {
+				count += m.get(x) * m.get(-x);
 			}
 		}
 		int ans = count / 2;
-		if(m.containsKey(0)) {
+		if (m.containsKey(0)) {
 			int n = m.get(0);
-			count += n * (n+1) / 2;
+			ans += n * (n - 1) / 2;
 		}
-		
-		
-		return count;
+
+		return ans;
 	}
 }
