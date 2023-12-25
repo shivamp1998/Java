c46e528c57411430999ae192a3b07853f14f5066
shivamp1998 <shivampandey98@gmail.com>
2023-12-26 02:37:46 +0530

    fixes

--- src/Java/HeapSort.java
+++ src/Java/HeapSort.java
@@ -1,31 +1,32 @@
 package Java;
 
 public class HeapSort {
-    public static int [] heapify(int arr[],int index) {
-		for(int i=0; i<=index; i++) {
-			int childIndex = i;
-				while(childIndex > 0) {
-					int parentIndex = (childIndex - 1) / 2;
-					if(arr[parentIndex] > arr[childIndex]) {
-						int temp = arr[parentIndex];
-						arr[parentIndex] = arr[childIndex];
-						arr[childIndex] = temp;
-						childIndex = parentIndex;
-					}else{
-						break;
-					}
-				}
-		}
-			return arr; 
-	}
-	public static void inplaceHeapSort(int arr[]) {
-		int index = arr.length - 1;
-		while(index > 0) {
-			arr = heapify(arr, index);
-			int temp = arr[0];
-			arr[0] = arr[index];
-			arr[index] = temp;
-			index--;
-		}
-	}
+    public static int[] heapify(int arr[], int index) {
+        for (int i = 0; i <= index; i++) {
+            int childIndex = i;
+            while (childIndex > 0) {
+                int parentIndex = (childIndex - 1) / 2;
+                if (arr[parentIndex] > arr[childIndex]) {
+                    int temp = arr[parentIndex];
+                    arr[parentIndex] = arr[childIndex];
+                    arr[childIndex] = temp;
+                    childIndex = parentIndex;
+                } else {
+                    break;
+                }
+            }
+        }
+        return arr;
+    }
+
+    public static void inplaceHeapSort(int arr[]) {
+        int index = arr.length - 1;
+        while (index > 0) {
+            arr = heapify(arr, index);
+            int temp = arr[0];
+            arr[0] = arr[index];
+            arr[index] = temp;
+            index--;
+        }
+    }
 }
